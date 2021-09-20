f3cb797a3c08ebd39e16421a88ea711c502756af
OlhovikM <Olhovikmaria14@gmail.com>
2021-09-20 12:36:42 +0300

    Initial commit

---
+++ com/company/mki4/SumNumb.java
@@ -0,0 +1,11 @@
+package com.company.mki4;
+public class SumNumb {
+    public static void main(String[] args) {
+        printSum(10, 15);
+    }
+    public static void printSum(int numberOne, int numberTwo){
+        System.out.println("10+15="+(numberOne+numberTwo));
+    }
+}
+
+
